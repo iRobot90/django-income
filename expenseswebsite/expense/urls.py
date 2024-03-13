f7b62929d42a6ccf8962fd8031acf499fadd2d1c
iRobot90 <amiandajesse27@gmail.com>
2024-03-13 22:26:52 +0300

    first view

---
+++ expenseswebsite/expense/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views
+
+
+
+urlpatterns = [
+ 
+    path('', views.index, name='expenses'),
+    path('add-expenses', views.add_expense, name='add expense'),
+
+]
